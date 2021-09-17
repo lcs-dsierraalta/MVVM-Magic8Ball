ef08ed399427b47ac717b04b4266c02306518dd9
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-09-17 11:56:26 -0400

    Added a model file to separate responses data from the interface logic.

--- MVVM-Magic8Ball/main.swift
+++ MVVM-Magic8Ball/main.swift
@@ -32,38 +32,9 @@ print("")
 // TIP: Use the library to get code templates
 //      Command-Shift-L
 //
+
 var response = ""
 
-let responses = [
-    
-// Affirmative answers
-    "It is certain",
-    "It is decidely so.",
-    "Without a doubt.",
-    "Yes definitely.",
-    "You may rely on it.",
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good.",
-    "Yes.",
-    "Signs point to yes.",
-    
-// Non-committal answers
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not to tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-    
-// Negative answers
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful.",
-    
-]
-
-
 // Provide the advice
-print(responses.randomElement()!)
+let advisor = Magic8Ball()
+print(advisor.getResponse())
