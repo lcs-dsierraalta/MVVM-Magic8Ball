18b6984c65e1aa61c481362d8a5b8d6b89238e16
Diego Sierraalta <dsierraalta@lcs.on.ca>
2021-09-20 14:50:19 -0400

    Added a while loop so that the user is able to continue asking questions without having the program ending.
    Also added an iOS target to begin making an iOS version of this program using swiftUI

--- macOS/Views/main.swift
+++ macOS/Views/main.swift
@@ -14,22 +14,25 @@ var advisor = AdviceViewModel()
 print("MAGIC 8 BALL")
 print("============")
 
-// Ask for user's question
-print("""
+while true {
+    
+    // Ask for user's question
+    print("""
 NOTE: Questions should be phrased such
       that they can be answered with a
       yes or no response.
 
 """)
-print("What is your question? ")
-// Get input as a non-optional String
-let input = readLine()!
-
-// Print out their question
-print("")
-print("You said: \(input)")
-print("")
-
-
-// Provide the advice
-print(advisor.provideResponseFor(givenQuery: input))
+    print("What is your question? ")
+    // Get input as a non-optional String
+    let input = readLine()!
+    
+    // Print out their question
+    print("")
+    print("You said: \(input)")
+    print("")
+    
+    
+    // Provide the advice
+    print(advisor.provideResponseFor(givenQuery: input))
+}
